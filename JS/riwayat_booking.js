c61fcf18caeaf136006540114f9926bc098c05e7
alauerisreal007 <azrulretropus200@gmail.com>
2023-10-10 14:22:16 +0700

    revisi 33

--- JS/riwayat_booking.js
+++ JS/riwayat_booking.js
@@ -13,12 +13,21 @@ function riwayat() {
                 email: email.value,
                 service: service.value
             })
+        // .then(function (response) {
+        //     if (response.ok) {
+        //         return response.json();
+        //     }
+        //     return Promise.reject(response);
+            .then(response => response.json())
+            .then(data => {
+            const bodyElement = document.querySelector('body pre');
+            bodyElement.querySelector('nama').textContent = data.nama;
+            bodyElement.querySelector('noHP').textContent = data.numPhone;
+            bodyElement.querySelector('email').textContent = data.email;
+            bodyElement.querySelector('service').textContent = data.services;
+            bodyElement.querySelector('note').textContent = data.note;
+            bodyElement.querySelector('date').textContent = data.date;
         })
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(response);
         }).then(function (data) {
             alert(data.message);
             if (confirm(data.message)) {
@@ -28,3 +37,5 @@ function riwayat() {
             console.log(error);
         })
     }
+
+document.addEventListener('DOMContentLoaded', riwayat);
